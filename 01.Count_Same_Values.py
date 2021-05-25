12bbbe68f5b18918d61d1e6cc9a3c5513d21c7a5
Ivaylo Ivanov <64315685+negative0101@users.noreply.github.com>
2021-05-25 19:51:32 +0300

    Add files via upload

---
+++ 01.Count_Same_Values.py
@@ -0,0 +1,10 @@
+nums = tuple(float(i) for i in input().split())
+
+result = {}
+for num in nums:
+    if num not in result:
+        result[num]= 0
+    result[num] +=1
+
+for key,value in result.items():
+    print(f'{key} - {value} times')
