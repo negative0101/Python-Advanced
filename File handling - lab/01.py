a53e1a9c899d38d9c088fabb1face933d426b626
Ivaylo Ivanov <64315685+negative0101@users.noreply.github.com>
2021-06-08 20:23:37 +0300

    Add files via upload

---
+++ File handling - lab/01.py
@@ -0,0 +1,14 @@
+from os import path
+
+#First
+if path.exists('file.txt'):
+    print('File found')
+else:
+    print('File not found')
+
+#Second
+try:
+    open('file.txt')
+    print('File found')
+except FileNotFoundError:
+    print('File not found')
